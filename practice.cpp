812b565d5eb7ee34a68fd49f8a69145d3ccb9323
Robyn Collins <robyn.collins@absio.com>
2020-12-07 12:30:26 -0800

    first push

---
+++ practice.cpp
@@ -0,0 +1,5 @@
+//
+// Created by meowc on 12/7/2020.
+//
+
+#include "practice.h"
---
+++ practice.h
@@ -0,0 +1,14 @@
+//
+// Created by meowc on 12/7/2020.
+//
+
+#ifndef CPPSTUDY_PRACTICE_H
+#define CPPSTUDY_PRACTICE_H
+
+
+class practice {
+
+};
+
+
+#endif //CPPSTUDY_PRACTICE_H
